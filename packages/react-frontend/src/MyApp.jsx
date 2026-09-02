de98a001a957c80922005750534d5222c609b044
ubuntulaptop <garrettwjc@gmail.com>
2026-09-02 16:45:40 -0700

    Table Done

--- packages/react-frontend/src/MyApp.jsx
+++ packages/react-frontend/src/MyApp.jsx
@@ -1,28 +1,73 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Table from "./Table";
 import Form from "./Form";
 
 function MyApp() {
-  const [characters, setCharacters] = useState([]);
+	const [characters, setCharacters] = useState([]);
 
-  function removeOneCharacter(index) {
-    const updated = characters.filter((character, i) => i !== index);
-    setCharacters(updated);
-  }
+	function fetchUsers() {
+		return fetch("http://localhost:8000/users");
+	}
 
-  function updateList(person) {
-    setCharacters([...characters, person]);
-  }
+	function postUser(person) {
+		return fetch("http://localhost:8000/users", {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json"
+			},
+			body: JSON.stringify(person)
+		});
+	}
 
-  return (
-    <div className="container">
-      <Table
-        characterData={characters}
-        removeCharacter={removeOneCharacter}
-      />
-      <Form handleSubmit={updateList} />
-    </div>
-  );
+	function deleteUser(id) {
+		return fetch(`http://localhost:8000/users/${id}`, {
+			method: "DELETE"
+		});
+	}
+
+	function updateList(person) {
+		postUser(person)
+			.then((response) => {
+				if (response.status !== 201) {
+					throw new Error("User was not created");
+				}
+
+				return response.json();
+			})
+			.then((createdUser) => {
+				setCharacters([...characters, createdUser]);
+			})
+			.catch((error) => console.log(error));
+	}
+
+	function removeOneCharacter(person) {
+		deleteUser(person.id)
+			.then((response) => {
+				if (response.status === 204) {
+					setCharacters(
+						characters.filter((character) => character.id !== person.id)
+					);
+				}
+			})
+			.catch((error) => console.log(error));
+	}
+
+	useEffect(() => {
+		fetchUsers()
+			.then((res) => res.json())
+			.then((json) => setCharacters(json.users_list))
+			.catch((error) => console.log(error));
+	}, []);
+
+	return (
+		<div className="container">
+			<Table
+				characterData={characters}
+				removeCharacter={removeOneCharacter}
+			/>
+			<Form handleSubmit={updateList} />
+		</div>
+	);
 }
 
 export default MyApp;
